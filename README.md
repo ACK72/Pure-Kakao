4588c2e8363d184bd33bebc7d92fdc436b68e494
ACK72 <acikom72@gmail.com>
2022-11-27 03:59:07 +0900

    v0.1.0 release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pure-Kakao
+
+Simple & lightweight ad-remover for kakaotalk pc client, written in Rust
+
+![](https://user-images.githubusercontent.com/25812442/204104733-9be014bd-3978-4842-8ae0-f3f99d497e52.png)
+
+## Download
+
+- You can get latest builds at [release page](https://github.com/ACK72/Pure-Kakao/releases/latest)
+
+## Limitations
+
+- Not support blocking pop-up ads, as these are rare, and it requires "expensive" search loop
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    thread, time::Duration
+};
+use windows::{
+    core::*, Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*
+};
+
+fn main() {
+    let title = w!("카카오톡");
+
+    let mut hwnd;
+    let mut frame;
+
+    loop {
+        hwnd = HWND(0);
+        frame = RECT::default();
+
+        let handle = unsafe { FindWindowW(PCWSTR::null(), title) };
+        let _ = unsafe { GetWindowRect(handle, &mut frame) };
+        let kt_size = frame.bottom - frame.top;
+
+        let ad_hwnd = unsafe { FindWindowExA(handle, HWND(0), s!("BannerAdWnd"), PCSTR::null()) };
+        let _ = unsafe { GetWindowRect(ad_hwnd, &mut frame) };
+        let ad_size = frame.bottom - frame.top;
+
+        unsafe {
+            ShowWindow(ad_hwnd, SW_HIDE);
+        }
+
+        loop {
+            hwnd = unsafe { FindWindowExA(handle, hwnd, PCSTR::null(), PCSTR::null()) };
+
+            if hwnd == HWND(0) {
+                break;
+            }
+
+            let size = unsafe { GetWindowTextLengthW(hwnd) } as usize;
+            let mut caption_vec = vec![0 as u16; size];
+            let _ = unsafe { GetWindowTextW(hwnd, &mut caption_vec) };
+            let caption = String::from_utf16(&mut caption_vec).unwrap();
+
+            if caption.starts_with("OnlineMainView") || caption.starts_with("LockModeView") {
+                let _ = unsafe { GetWindowRect(hwnd, &mut frame) };
+                let wn_size = frame.bottom - frame.top;
+
+                if wn_size != 0 {
+                    let space = if kt_size > wn_size + ad_size {
+                        kt_size - wn_size - ad_size
+                    } else {
+                        0
+                    };
+
+                    if space + wn_size + ad_size <= kt_size {
+                        unsafe {
+                            SetWindowPos(hwnd, HWND(0), 0, 0, frame.right - frame.left, wn_size + ad_size + 1, SWP_NOZORDER | SWP_NOMOVE);
+                        }
+                    }
+                }
+            }
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
+}
